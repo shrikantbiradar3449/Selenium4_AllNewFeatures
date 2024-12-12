f22566b38176af6a828ab847d1ef2543bec4bc3a
Shrikant Biradar <shrikantbiradar.study@gmail.com>
2024-12-12 23:04:06 +0530

    Updated POM for selenium and code to launch url in selenium 4.27.0

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,29 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+//Selenium  Manager in build driver download based on chrome version
+        // System path for ChromeDriver needs to be set before running the code
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/Drivers/chromedriver.exe");
+
+        // Create a new WebDriver instance
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        // Open the desired URL
+        driver.get("https://practicesoftwaretesting.com/");
+
+        // Maximize the browser window (optional)
+        driver.manage().window().maximize();
+
+        // Pause the execution for a few seconds (optional)
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Close the browser window
+        driver.quit();
     }
 }
